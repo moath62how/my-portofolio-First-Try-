4005df98d9832a41dba01db5cd713d07f4376ee9
moath62how <moath26how@gmail.com>
2022-09-06 18:12:22 +0200

    did some javascript work still needs effort

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var myLANG = ["javascript", "C++", "Python", "CSS", "HTML"]
 var myLANGp = [65, 10, 25, 89, 95]
-const markup = `<div id="progress-bar"><span></span> `
-// document.getElementById('bars').innerHTML = markup.repeat(5)
-// for () { }
+const markup = `<div ="bars"><div id="progress-bar"><span></span></div> `
+//document.getElementById('DOM').innerHTML = markup.repeat(myLANG.length)
+
+for (let i = 0; i < myLANG.length; i++) {
+    document.getElementById('DOM').innerHTML = `<div ="bars"><div id="progress-bar"><span>${myLANGp[i]}%</span></div>`
+}
