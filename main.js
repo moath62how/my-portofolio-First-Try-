999587b2f8db3763e588dae196a375df74798755
moath62how <moath26how@gmail.com>
2022-09-06 22:28:07 +0200

    made the cards bigger

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var myLANG = ["javascript", "C++", "Python", "CSS", "HTML"]
+var myLANG = ["Javascript", "C++", "Python", "CSS", "HTML"]
 var myLANGp = [65, 10, 25, 89, 95]
 const markup = `<div ="bars"><div id="progress-bar"><span></span></div> `
 //document.getElementById('DOM').innerHTML = markup.repeat(myLANG.length)
 
 for (let i = 0; i < myLANG.length; i++) {
-    document.getElementById('DOM').innerHTML += `<div ="bars">${myLANG[i]}<div id="progress-bar"><span id="pbar${i}">${myLANGp[i]}% </span></div></div>`
+    document.getElementById('DOM').innerHTML += `<div ="bars">${myLANG[i]} :<div id="progress-bar"><span id="pbar${i}">${myLANGp[i]}% </span></div></div>`
 }
 for (let j = 0; j < myLANG.length; j++) {
     document.getElementById(`pbar${j}`).style.width = `${myLANGp[j]}%`
