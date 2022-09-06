c8cbab7e3931cdde1a804da9eb92138135aeb4bc
moath62how <moath26how@gmail.com>
2022-09-06 22:24:40 +0200

    added name for the progress bar

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const markup = `<div ="bars"><div id="progress-bar"><span></span></div> `
 //document.getElementById('DOM').innerHTML = markup.repeat(myLANG.length)
 
 for (let i = 0; i < myLANG.length; i++) {
-    document.getElementById('DOM').innerHTML += `<div ="bars"><div id="progress-bar"><span id="pbar${i}">${myLANGp[i]}%</span></div>`
+    document.getElementById('DOM').innerHTML += `<div ="bars">${myLANG[i]}<div id="progress-bar"><span id="pbar${i}">${myLANGp[i]}% </span></div></div>`
 }
 for (let j = 0; j < myLANG.length; j++) {
     document.getElementById(`pbar${j}`).style.width = `${myLANGp[j]}%`
+    document.getElementById(`pbar${j}`).style.textAlign = `center`
+    document.getElementById(`pbar${j}`).style.overflow = "visible"
+    // window.getComputedStyle(`pbar${j}`, `::after`).content = `${myLANG[j]}`
 }
