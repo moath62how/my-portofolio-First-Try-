1e18f1e94b3a4aee73e3538f46673913e7ada9bd
moath62how <moath26how@gmail.com>
2022-09-06 17:57:54 +0200

    some small touches

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var myLANG = ["javascript", "C++", "Python", "CSS", "HTML"]
-var myLANGp = []
+var myLANGp = [65, 10, 25, 89, 95]
 const markup = `<div id="progress-bar"><span></span> `
 // document.getElementById('bars').innerHTML = markup.repeat(5)
 // for () { }
