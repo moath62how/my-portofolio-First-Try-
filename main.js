39b60080af877c5db2192e5cfe7457d3cf538870
moath62how <moath26how@gmail.com>
2022-09-06 18:25:15 +0200

    dynamic lang knowen code

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ const markup = `<div ="bars"><div id="progress-bar"><span></span></div> `
 //document.getElementById('DOM').innerHTML = markup.repeat(myLANG.length)
 
 for (let i = 0; i < myLANG.length; i++) {
-    document.getElementById('DOM').innerHTML += `<div ="bars"><div id="progress-bar"><span id="">${myLANGp[i]}%</span></div>`
+    document.getElementById('DOM').innerHTML += `<div ="bars"><div id="progress-bar"><span id="pbar${i}">${myLANGp[i]}%</span></div>`
+}
+for (let j = 0; j < myLANG.length; j++) {
+    document.getElementById(`pbar${j}`).style.width = `${myLANGp[j]}%`
 }
