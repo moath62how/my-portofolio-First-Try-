8eb7edc8cc552ea968a458b22d3cddb9e88bd2ae
moath62how <moath26how@gmail.com>
2022-09-06 17:52:52 +0200

    js is looking lit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var myLANG = ["javascript", "C++", "Python", "CSS", "HTML"]
-
+var myLANGp = []
 const markup = `<div id="progress-bar"><span></span> `
 // document.getElementById('bars').innerHTML = markup.repeat(5)
+// for () { }
